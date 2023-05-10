02f399b6ba574ed3180fe4670832b7407efac6a4
07Vaishnavi-Singh <100015197+07Vaishnavi-Singh@users.noreply.github.com>
2023-05-10 15:54:12 +0530

    checker added

--- checker.js
+++ checker.js
@@ -3,5 +3,40 @@ const { Network, Alchemy } = require("alchemy-sdk");
 const { main } = require("./index.js");
 const mysql = require("mysql2");
 
+const settings = {
+    apiKey: "IL1vdeNVYypZ2yz7Xgqhbh8bydBjgzOR", 
+    network: Network.ETH_SEPOLIA, 
+  };
 
+const addresses = main();
 
+const alchemy = new Alchemy(settings);
+
+const provider = new AlchemyProvider(
+    "ETH_SEPOLIA",
+    "IL1vdeNVYypZ2yz7Xgqhbh8bydBjgzOR"
+  );
+
+  const insertTransactionFunction = (transaction) => {
+    const connection = mysql.createConnection({
+      host: "localhost",
+      user: "username",
+      password: "password",
+      database: "database",
+    });
+
+    connection.connect();
+
+    const sql =
+      "insert into transactions (BlockNum,Transaction_Hash,FromAddress,ToAddress,Amount) VALUES (?, ?, ?, ?, ?)";
+    const params = [parseInt(transaction.blockNum,16),transaction.hash,transaction.from, transaction.to, transaction.value];
+  
+    connection.query(sql, params, function (error, results, fields) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(`Transaction inserted: ${transaction.hash}`);
+      }
+      connection.end();
+    });
+  };
